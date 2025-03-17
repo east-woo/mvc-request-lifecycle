4fb11df36a438f31f02487c3bff72e12b71af165
dongwoo <dwseo96@wavus.co.kr>
2025-03-17 21:31:21 +0900

    Initial commit

---
+++ src/main/java/com/wavus/edu/mvcrequestlifecycle/MvcRequestLifecycleApplication.java
@@ -0,0 +1,13 @@
+package com.wavus.edu.mvcrequestlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRequestLifecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcRequestLifecycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wavus/edu/mvcrequestlifecycle/MvcRequestLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavus.edu.mvcrequestlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRequestLifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
